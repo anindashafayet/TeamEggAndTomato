a5b7e1e3f133b12f6cfab6ed3cdd422895ec3640
zhubbb <zhubbb@hotmail.com>
2017-10-08 19:39:57 -0400

    git reclaim

---
+++ BlueTomato/db/migrate/20171007010106_create_accounts.rb
@@ -0,0 +1,18 @@
+class CreateAccounts < ActiveRecord::Migration[5.1]
+  def change
+    create_table :accounts do |t|
+      #t.belongs_to :accountable, index: {unique: true}, foreign_key: true
+      t.text :email
+      t.text :username
+      t.binary :password
+      t.text :privilege
+      t.integer :timestamp
+      t.binary :salt
+      t.integer :accountable_id
+      t.string :accountable_type
+
+      t.timestamps
+    end
+    add_index :accounts, [:accountable_id, :accountable_type], :unique => true
+  end
+end
