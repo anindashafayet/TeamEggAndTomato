89012b7878276ca03048d89984bbdf139e479d2f
carlhejiayu <carlhejiayu@gmail.com>
2017-10-08 16:35:58 -0400

    add the some view page for profile

--- BlueTomato/app/controllers/profiles_controller.rb
+++ BlueTomato/app/controllers/profiles_controller.rb
@@ -1,2 +1,5 @@
 class ProfilesController < ApplicationController
+  def new
+
+  end
 end
--- BlueTomato/config/routes.rb
+++ BlueTomato/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'teenagers/new'
   resources :teenagers
+  resources :profiles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
