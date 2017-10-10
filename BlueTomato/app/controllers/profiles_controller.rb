35c0bb455459f535bdbb9e8c3b9af0de5a1a7b11
carlhejiayu <carlhejiayu@gmail.com>
2017-10-10 09:58:18 -0400

    Basic Profile skeleton

--- BlueTomato/app/controllers/profiles_controller.rb
+++ BlueTomato/app/controllers/profiles_controller.rb
@@ -2,4 +2,21 @@ class ProfilesController < ApplicationController
   def new
 
   end
+
+  def create
+    #we need to find whether this profile linked to the account infomation
+    @profile = Profile.new(profile_params)
+    @profile.save
+    redirect_to @profile
+  end
+
+  def show
+    @profile = Profile.find(params[:id])
+  end
+
+  private
+  def profile_params
+    params.require(:profile).permit(:description, :phone , :address)
+  end
+
 end
