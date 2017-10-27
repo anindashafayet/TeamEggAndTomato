08bb088b96128c8d17b7d08a6b7ba7c133675379
Aninda Shafayet <aninda.shafayet@mail.utoronto.ca>
2017-10-27 12:01:02 -0400

    Adding Home of website and links between its pages.

---
+++ BlueTomato/app/controllers/home_controller.rb
@@ -0,0 +1,31 @@
+class HomeController < ApplicationController
+  def home
+  end
+
+  def signup
+  end
+
+  def login
+  end
+
+  def aboutus
+  end
+
+  def profile
+  end
+
+  def services
+  end
+  
+  def faq
+  end
+  
+  def tos
+  end
+  
+  def pp
+  end
+  
+  def sitemap
+  end
+end
--- BlueTomato/config/routes.rb
+++ BlueTomato/config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get 'home/home', to: "home#home", as: "home"
+  get 'home/signup', to: "home#signup", as: "signup"
+  get 'home/login', to: "home#login", as: "login"
+  get 'home/aboutus', to: "home#aboutus", as: "aboutus"
+  get 'home/profile', to: "home#profile", as: "profile"
+  get 'home/services', to: "home#services", as: "services"
+  get 'home/faq', to: "home#faq", as: "faq"
+  get 'home/tos', to: "home#tos", as: "tos"
+  get 'home/pp', to: "home#pp", as: "pp"
+  get 'home/sitemap', to: "home#sitemap", as: "sitemap"
+  get 'teenagers/new', to: "teenagers#new", as: "new"
+
   get 'customer_service/index'
   post 'customer_service/submit_feedback'#, to: 'customer_service#submit_feedback'
   #get 'customer_service/submit_feedback'#, to: 'customer_service#submit_feedback'
