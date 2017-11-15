1b16708c2991b87af0b78a2d6f2596b04628f1a1
ewonguoft <eltong.wong@mail.utoronto.ca>
2017-11-15 10:14:54 -0500

    removing old login from page

--- BlueTomato/db/schema.rb
+++ BlueTomato/db/schema.rb
@@ -108,7 +108,7 @@ ActiveRecord::Schema.define(version: 20171115071213) do
     t.integer "account_id"
     t.integer "service_type_id"
     t.date "period"
-    t.string "period_detail", default: "N/A"
+    t.string "period_detail"
     t.text "detail"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
