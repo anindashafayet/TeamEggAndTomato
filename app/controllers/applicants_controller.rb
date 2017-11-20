79acc2b1ef70d9a9c1e661858e5e32b8d0ff5e43
heherbertzhang <heherbert.zhang@mail.utoronto.ca>
2017-11-19 19:45:36 -0500

    fix bug in apply job

--- app/controllers/applicants_controller.rb
+++ app/controllers/applicants_controller.rb
@@ -9,7 +9,7 @@ class ApplicantsController < ApplicationController
           @applicant = Applicant.new()
           @applicant.errors.add(:base, :invalid, message:"Cannot apply as request owner.")
           flash[:notice] = "Cannot apply as request owner."
-      elsif @client_request.applicants.exists?(user_id: logged_in_user_or_guest().id)
+      elsif !@client_request.applicants.exists?(user_id: logged_in_user_or_guest().id)
           @applicant = @client_request.applicants.create({user_id: logged_in_user_or_guest().id, \
               client_request_id:params[:client_request_id]})
           config.logger = Logger.new(STDOUT)
