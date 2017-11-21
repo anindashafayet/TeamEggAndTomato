5e01d25d4c60850eeeab8c9ab3a9aa4c0bff6596
heherbertzhang <heherbert.zhang@mail.utoronto.ca>
2017-11-21 15:33:35 -0500

    rank the posting on home page based on hottness and time

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def home
-    @client_requests = ClientRequest.all
+    @client_requests = ClientRequest.all.sort_by{|m| [m.applicants.count(), m.created_at]}[0,5]
   end
 
   def signup
@@ -17,16 +17,16 @@ class HomeController < ApplicationController
 
   def services
   end
-  
+
   def faq
   end
-  
+
   def tos
   end
-  
+
   def pp
   end
-  
+
   def sitemap
   end
 end
